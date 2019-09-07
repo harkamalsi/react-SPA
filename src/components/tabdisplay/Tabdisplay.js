f6d81ad2c74081883ae1cb1c5494070651ec2476
harkamalsi <harkamal_guraya@hotmail.com>
2019-09-07 23:00:46 +0200

    Jobbet med CSS til tabdisplay.

--- src/components/tabdisplay/Tabdisplay.js
+++ src/components/tabdisplay/Tabdisplay.js
@@ -4,10 +4,18 @@ import './Tabdisplay.css';
 function Tabdisplay(props) {
   return (
     <div className='Tabdisplay'>
-      <div className='combination'>Kombinasjon1</div>
-      <div className='combination'>Kombinasjon2</div>
-      <div className='combination'>Kombinasjon3</div>
-      <div className='combination'>Kombinasjon4</div>
+      <div className='combination' id='combination1'>
+        Kombinasjon1
+      </div>
+      <div className='combination' id='combination2'>
+        Kombinasjon2
+      </div>
+      <div className='combination' id='combination3'>
+        Kombinasjon3
+      </div>
+      <div className='combination' id='combination4'>
+        Kombinasjon4
+      </div>
     </div>
   );
 }
