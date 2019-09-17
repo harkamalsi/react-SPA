3ddcfbc19cc8e82b57e56f9075cfe42114fa2bef
Emanuele <emanueca@stud.ntnu.no>
2019-09-17 20:55:20 +0200

    Cleaned source code

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
 import {Soundform} from '../soundform/Soundform';
+import ChoiceSelector from '../choiceSelector/ChoiceSelector';
+
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: []
+      //here are all the category retrieved from the child componets and stored in the state of the parent sidebar
+        // and later on passed to the app in order to be retrieve the data 
+      pictureCategory : null,
+      textCategory : null,
+      soundCategory : null, 
     };
+    //this.updateTextData = this.updateTextData.bind(this);
+  }
+  updatePictureCategory = (picture) =>{
+    this.setState({
+      pictureCategory : picture,
+    });
+  }
+  updateTextCategory = (text) => {
+    this.setState({
+      textCategory : text,
+    });
   }
 
   render() {
@@ -15,6 +32,8 @@ class Sidebar extends Component {
       <div className='Sidebar'>
         <h3>Kategorier</h3>
         <Soundform/>
+        <ChoiceSelector sendCategory = {this.updateTextCategory} categoryName="Text" alternative1="Christmas" alternative2="Valentine" alternative3="Get Well"/>
+        <ChoiceSelector sendCategory = {this.updatePictureCategory} categoryName="Picture" alternative1="Dogs" alternative2="Cats" alternative3="Rats"/>
       </div>
     );
   }
