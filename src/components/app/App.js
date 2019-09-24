4c7fdd28e014cfe20103599fb1f1b310c2c43075
harkamalsi <harkamal_guraya@hotmail.com>
2019-09-24 21:14:46 +0200

    Working with undo-redo and sidebar. Not finished yet.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -17,10 +17,12 @@ class App extends React.Component {
       selectedTab: null,
       combinations: null
     };
+    // first undoArr length and then redoArrLength
   }
 
   componentDidUpdate = () => {
     console.log(
+      'APP state updated:',
       this.state.soundCategory,
       this.state.textCategory,
       this.state.pictureCategory
@@ -36,21 +38,23 @@ class App extends React.Component {
     // e.target.value will help us decide which comibation to show on the mainDisplay component.
     this.setState({ selectedTab: e.target.value });
   };
+
   updateTextCategory = text => {
     this.setState({
       textCategory: text
     });
   };
+
   updatePictureCategory = picture => {
     this.setState({
       pictureCategory: picture
     });
   };
+
   updateSoundCategory = sound => {
     this.setState({
       soundCategory: sound
     });
-    this.handleSession();
   };
 
   // save the categories chosen in the sidebar in the undo stack
@@ -74,45 +78,45 @@ class App extends React.Component {
 
     // categories should be saved in sessioStorage first when they are displayed. soundCategory could also be textCategory or pictureCategory.
     if (categories.soundCategory) {
-      if (sessionStorage.getItem('categoriesUndo')) {
-        if (categories.soundCategory) {
-          // push the element to the end of undoArr
-          undoArr.push(categories);
-          sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
-          console.log('new categories-element pushed into undoArr');
-        }
-      } else {
-        // push the element to the end of undoArr
-        undoArr.push(categories);
-        sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
-        console.log('new categories-element pushed into undoArr');
-      }
+      // push the element to the end of undoArr
+      undoArr.push(categories);
+      sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
+      // empty the redoArr
+      sessionStorage.setItem('categoriesRedo', JSON.stringify([]));
+      console.log(
+        categories.soundCategory,
+        categories.textCategory,
+        categories.pictureCategory,
+        '- pushed into undoArr'
+      );
     }
+
     console.log('undoArrStack: ', undoArr);
+    console.log(
+      'RedoArrStack: ',
+      JSON.parse(sessionStorage.getItem('categoriesRedo'))
+    );
   };
 
   handleUndo = () => {
     // There must be an undo array after handleSession()
     let undoArr = JSON.parse(sessionStorage.getItem('categoriesUndo'));
-    let redoArr = JSON.parse(sessionStorage.getItem('categoriesRedo'));
+    let redoArr = sessionStorage.getItem('categoriesRedo')
+      ? JSON.parse(sessionStorage.getItem('categoriesRedo'))
+      : [];
 
-    if (!redoArr) {
-      if (undoArr && undoArr.length > 0) {
-        // pops the last element in undoArr
-        let redoArr = [undoArr.pop()];
-        sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
-        sessionStorage.setItem('categoriesRedo', JSON.stringify(redoArr));
-        console.log('undoArr popped into newly created redoArr');
-      }
-    } else {
-      if (undoArr && undoArr.length > 0) {
-        let redoArr = JSON.parse(sessionStorage.getItem('categoriesRedo'));
-        // pops the last element in redoArr
-        redoArr.push(undoArr.pop());
-        sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
-        sessionStorage.setItem('categoriesRedo', JSON.stringify(redoArr));
-        console.log('undoArr popped into previously created redoArr');
-      }
+    if (undoArr && undoArr.length > 0) {
+      // pushes the last element of undoArr in redoArr
+      let categoriesItem = undoArr.pop();
+      redoArr.push(categoriesItem);
+      sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
+      sessionStorage.setItem('categoriesRedo', JSON.stringify(redoArr));
+      console.log(
+        categoriesItem.soundCategory,
+        categoriesItem.textCategory,
+        categoriesItem.pictureCategory,
+        '- pushed into redoArr'
+      );
     }
 
     let undoArr2 = JSON.parse(sessionStorage.getItem('categoriesUndo'));
@@ -124,14 +128,22 @@ class App extends React.Component {
 
   // NOT FINISHED YET!!!!
   handleRedo = () => {
-    let redoArr = JSON.parse(sessionStorage.getItem('categoriesRedo'));
+    let redoArr = sessionStorage.getItem('categoriesRedo')
+      ? JSON.parse(sessionStorage.getItem('categoriesRedo'))
+      : [];
 
     if (redoArr && redoArr.length > 0) {
-      let undoArr = JSON.parse(sessionStorage.getItem('categoriesRedo'));
-      undoArr.push(redoArr.pop());
+      let undoArr = JSON.parse(sessionStorage.getItem('categoriesUndo'));
+      let categoriesItem = redoArr.pop();
+      undoArr.push(categoriesItem);
       sessionStorage.setItem('categoriesUndo', JSON.stringify(undoArr));
       sessionStorage.setItem('categoriesRedo', JSON.stringify(redoArr));
-      console.log('redoArr popped into undoArr');
+      console.log(
+        categoriesItem.soundCategory,
+        categoriesItem.textCategory,
+        categoriesItem.pictureCategory,
+        '- pushed into undoArr'
+      );
     }
 
     let undoArr3 = JSON.parse(sessionStorage.getItem('categoriesUndo'));
@@ -139,6 +151,32 @@ class App extends React.Component {
 
     console.log({ undoArr3 });
     console.log({ redoArr3 });
+  };
+
+  // Returns true if undoArr is empty and false else
+  isUndoEmpty = () => {
+    let length = sessionStorage.getItem('categoriesUndo')
+      ? JSON.parse(sessionStorage.getItem('categoriesUndo')).length
+      : 0;
+
+    let output = length === 0 ? true : false;
+
+    console.log('undoLengthEmpty: ', output);
+
+    return output;
+  };
+
+  // Returns true if redoArr is empty and false else
+  isRedoEmpty = () => {
+    let length = sessionStorage.getItem('categoriesRedo')
+      ? JSON.parse(sessionStorage.getItem('categoriesRedo')).length
+      : 0;
+
+    let output = length === 0 ? true : false;
+
+    console.log('redoLengthEmpty: ', output);
+
+    return output;
   };
 
   handleFavorite = () => {
@@ -187,6 +225,8 @@ class App extends React.Component {
                 onChange={this.handleSession}
                 handleUndo={this.handleUndo}
                 handleRedo={this.handleRedo}
+                isUndoEmpty={this.isUndoEmpty}
+                isRedoEmpty={this.isRedoEmpty}
                 updateTextCategory={this.updateTextCategory}
                 updatePictureCategory={this.updatePictureCategory}
                 updateSoundCategory={this.updateSoundCategory}
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,8 +3,21 @@ import ChoiceSelector from '../choiceSelector/ChoiceSelector';
 import './Sidebar.css';
 
 const Sidebar = props => {
+  let isUndoEmpty = props.isUndoEmpty();
+  let isRedoEmpty = props.isRedoEmpty();
+  console.log(isUndoEmpty, isRedoEmpty);
+
+  const updateButtonDisableValue = () => {
+    isUndoEmpty = props.isUndoEmpty();
+    isRedoEmpty = props.isRedoEmpty();
+  };
+
+  // works as componentDidUpdate for sidebar
   useEffect(() => {
     props.onChange();
+
+    // following makes sure to update the values for undoButton and redoButton
+    updateButtonDisableValue();
   });
 
   return (
@@ -32,13 +45,27 @@ const Sidebar = props => {
         alternative2='Cats'
         alternative3='Rats'
       />
-      <div className='undo-redo'>
-        <i onClick={props.handleUndo} class='material-icons'>
-          undo
-        </i>
-        <i onClick={props.handleRedo} class='material-icons'>
-          redo
-        </i>
+      <div className='undo-redo-buttons'>
+        <button
+          id='undo'
+          className='undo-redo-btn'
+          // disable the undo button if the length is <= 1. We don't want to undo the first default move; the first element is the default move.
+          onClick={props.handleUndo}
+          disabled={isUndoEmpty}
+        >
+          <i class='material-icons'>undo</i>
+          Angre
+        </button>
+        <button
+          id='redo'
+          className='undo-redo-btn'
+          // disable the redo button if the length is == 0. Then there is nothing to redo.
+          onClick={props.handleRedo}
+          disabled={isRedoEmpty}
+        >
+          <i class='material-icons'>redo</i>
+          Gjenta
+        </button>
       </div>
     </div>
   );
