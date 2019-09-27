07b3118d02221950c105b5a7f2cb28c0d5f48da9
Harkamaljit Singh <harkamas@stud.ntnu.no>
2019-09-27 11:48:44 +0200

    Favorite done

--- src/components/maindisplay/Maindisplay.js
+++ src/components/maindisplay/Maindisplay.js
@@ -1,54 +1,61 @@
 import React from 'react';
 import './Maindisplay.css';
 import AudioPlayer from '../soundform/AudioPlayer';
-import loading from './loading.svg'
+import loading from './loading.svg';
 
 const WelcomeMessage = props => {
   return (
     <div>
-      <h2>
-        Velkommen!
-      </h2>
-      <p>Vennligst velg tre kategorier per medietype i vindu til høyre og trykk på en tab for å lage en kombinasjon.</p>
-
+      <h2>Velkommen!</h2>
+      <p>
+        Vennligst velg tre kategorier per medietype i vindu til høyre og trykk
+        på en tab for å lage en kombinasjon.
+      </p>
     </div>
   );
 };
 
 const Maindisplay = props => {
-
   if (props.isWelcomeScreen)
-  return(
-    <WelcomeMessage
-      selectedTab={props.selectedTab}
-    />
-  )
+    return <WelcomeMessage selectedTab={props.selectedTab} />;
   else if (props.data === null)
-    return(
+    return (
       <div>
-        <div className="text">Laster inn data...</div>
-        <div className="picture"> <img src={loading} alt=""/></div>
+        <div className='text'>Laster inn data...</div>
+        <div className='picture'>
+          {' '}
+          <img src={loading} alt='' />
+        </div>
       </div>
-
-    )
-    else{ 
-  return(
-    <div>
-    <div className="title" dangerouslySetInnerHTML ={{__html: props.data[1][0]}}></div>
-    <div className="picture" dangerouslySetInnerHTML ={{__html: props.data[0]}}></div>
-    <div className="text" dangerouslySetInnerHTML ={{__html: props.data[1][1]}}></div>
-    <AudioPlayer
-        soundTrack={props.selectedTab}
-        soundCategory={props.soundCategory}
-      />
-       <button className='btn delete' onClick={props.getFavorites}>
+    );
+  else {
+    return (
+      <div>
+        <div
+          className='title'
+          dangerouslySetInnerHTML={{ __html: props.data[1][0] }}
+        ></div>
+        <div
+          className='picture'
+          dangerouslySetInnerHTML={{ __html: props.data[0] }}
+        ></div>
+        <div
+          className='text'
+          dangerouslySetInnerHTML={{ __html: props.data[1][1] }}
+        ></div>
+        <AudioPlayer
+          soundTrack={props.selectedTab}
+          soundCategory={props.soundCategory}
+        />
+        <button className='btn delete' onClick={props.getFavorites}>
           <h3>Hent favoritt kombinasjon</h3>
         </button>
         <button className='btn favorite' onClick={props.handleFavorite}>
           <h3>Lagre Kombinasjon</h3>
         </button>
-    </div>
-  );}
-} 
+      </div>
+    );
+  }
+};
 
 export default Maindisplay;
