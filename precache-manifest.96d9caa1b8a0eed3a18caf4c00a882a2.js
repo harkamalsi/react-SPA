5c20dcac2ba1fbcf5f8eaa36c364d639840ce3c6
harkamalsi <harkamal_guraya@hotmail.com>
2019-10-26 19:44:01 +0200

    Updates

---
+++ precache-manifest.96d9caa1b8a0eed3a18caf4c00a882a2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c39c4d56d4faa55391951f210ec867",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "cae53677f36c1044b11a",
+    "url": "/react-spa/static/css/main.2e19762f.chunk.css"
+  },
+  {
+    "revision": "ea02853e15127b5c8c23",
+    "url": "/react-spa/static/js/2.7e3e216b.chunk.js"
+  },
+  {
+    "revision": "cae53677f36c1044b11a",
+    "url": "/react-spa/static/js/main.045a5238.chunk.js"
+  },
+  {
+    "revision": "aaddd3bdb64d17cdf003",
+    "url": "/react-spa/static/js/runtime~main.7f3be2ab.js"
+  },
+  {
+    "revision": "993b93bb363f433c253a4cd81818ebe5",
+    "url": "/react-spa/static/media/1.993b93bb.mp3"
+  },
+  {
+    "revision": "dfd519a6f7aa60dc17db14fe22d1a69d",
+    "url": "/react-spa/static/media/1.dfd519a6.mp3"
+  },
+  {
+    "revision": "fcc7e3418383fc6aea762af87489c82a",
+    "url": "/react-spa/static/media/1.fcc7e341.mp3"
+  },
+  {
+    "revision": "1de7fc7a7880de53cc880b86a669c117",
+    "url": "/react-spa/static/media/2.1de7fc7a.mp3"
+  },
+  {
+    "revision": "2a503fe3c0a5bdcea4b0019ac81e4724",
+    "url": "/react-spa/static/media/2.2a503fe3.mp3"
+  },
+  {
+    "revision": "8b58876aa61ba8103508f61781142644",
+    "url": "/react-spa/static/media/2.8b58876a.mp3"
+  },
+  {
+    "revision": "cc4260c874fed6058e422e9cadd6bf21",
+    "url": "/react-spa/static/media/3.cc4260c8.mp3"
+  },
+  {
+    "revision": "cf9c32fd9e720e3684bf07f6407e6ccd",
+    "url": "/react-spa/static/media/3.cf9c32fd.mp3"
+  },
+  {
+    "revision": "dcd6b18a385b4b715e58cf6391f1946a",
+    "url": "/react-spa/static/media/3.dcd6b18a.mp3"
+  },
+  {
+    "revision": "56b65e2c12a2bf66e144c54a0e7fc9c0",
+    "url": "/react-spa/static/media/4.56b65e2c.mp3"
+  },
+  {
+    "revision": "f4ac247820fd634ad8569571d19e17db",
+    "url": "/react-spa/static/media/4.f4ac2478.mp3"
+  },
+  {
+    "revision": "f4f48c37f5101a8a8051923ab2be59cd",
+    "url": "/react-spa/static/media/4.f4f48c37.mp3"
+  },
+  {
+    "revision": "09cfe256212b76eed2c5e1cbde567ed0",
+    "url": "/react-spa/static/media/loading.09cfe256.svg"
+  }
+]);
